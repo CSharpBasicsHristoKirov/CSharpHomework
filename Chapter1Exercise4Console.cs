d4b39d25bd2bda6c839cd5141ed78ea10b173029
CSharpBasicsHristoKirov <hristo.kirov85@gmail.com>
2016-02-15 18:11:53 +0200

    First exercises

---
+++ Chapter1Exercise4Console.cs
@@ -0,0 +1,12 @@
+// file directory C:\Users\Chris\Desktop
+// compiler directory: c:\Windows\Microsoft.NET\Framework\v4.0.21006\csc.exe HelloCSharp.cs
+// executable: HelloCSharp.exe
+
+class HelloCSharp
+{
+	static void Main()
+	{
+		System.Console.WriteLine("First Program Executed from command line.");
+		System.Console.ReadLine();	
+	}
+}
---
+++ ProgrammingBasicsCSharp/ProgrammingBasicsCSharp/Chapter1Exercise4VS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingBasicsCSharp
+{
+    class Chapter1Exercise4VS
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Message to Standard Output.");
+        }
+    }
+}
